5c14d184e030272c593f3ca3e2d759a1a1312a57
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2023-02-16 16:28:40 -0600

    Squashed commit of the following:
    commit c42f744a51814380d13f6560ebaa22632d1bfa34
    Author: Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
    Date:   Thu Feb 16 16:24:32 2023 -0600
    
        stdev channel
    
        also various cleanup
    
    commit 5bd33959e8e1e16c61fd091f872226e3c51a520e
    Author: Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
    Date:   Thu Feb 16 14:19:48 2023 -0600
    
        cleanup
    
    commit 8ed5fffcea46485ead9afd7c257e25d5247c7513
    Author: Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
    Date:   Thu Feb 16 08:48:42 2023 -0600
    
        measure works
    
        many messages unimplemented

--- README.md
+++ README.md
@@ -7,8 +7,14 @@
 [![ver](https://img.shields.io/badge/calver-YYYY.M.MICRO-blue)](https://calver.org/)
 [![log](https://img.shields.io/badge/change-log-informational)](https://github.com/yaq-project/yaqd-vimbax/-/blob/main/CHANGELOG.md)
 
-Daemons for cameras using the Generic Interface for Cameras ([GenICam](https://www.baslerweb.com/en/vision-campus/interfaces-and-standards/genicam-standard/)).  Uses the Python API for VimbaX.
+Daemons wrapping the [Vimba X SDK](https://www.alliedvision.com/en/products/software/vimba-x-sdk/). Vimba X is fully ([GenICam](https://www.baslerweb.com/en/vision-campus/interfaces-and-standards/genicam-standard/)) compliant.
 
 This package contains the following daemon(s):
 
 - https://yaq.fyi/daemons/vimbax-triggered
+
+
+## Installation Notes
+1. Download and install the [Vimba X SDK](https://www.alliedvision.com/en/products/software/vimba-x-sdk/) from the Allied Vision web page.
+2. Install the `vmbpy` package (not to be confused with [VimbaPython](https://github.com/alliedvision/VimbaPython), which is an older API). At the time of this writing, vmbpy was not available on Github, conda, or pip (though documentation suggests Allied Vision intends to host it on Github).  The package wheel can be found in the Vimba X program directory. 
+3. Install `yaqd-vimbax`.
---
+++ yaqd_vimbax/_triggered.py
@@ -0,0 +1,87 @@
+__all__ = ["Triggered"]
+
+import asyncio
+import numpy as np
+import time
+import vmbpy
+
+from typing import Dict, Any, List, Optional
+from yaqd_core import HasMeasureTrigger
+
+
+class Triggered(HasMeasureTrigger):
+    _kind = "vimbax-triggered"
+
+    def __init__(self, name, config, config_filepath):
+        super().__init__(name, config, config_filepath)
+        self._channel_names = ["mean", "stdev"]
+        self._channel_shapes = {k:(608, 808) for k in self._channel_names}
+        self._channel_units = {k:None for k in self._channel_names}
+
+    async def update_state(self):
+        with vmbpy.VmbSystem.get_instance() as vmb:
+            if self._config["serial"]:
+                camera_id = self._config["serial"]
+            else:
+                cams = vmb.get_all_cameras()                    
+                camera_id = cams[0].get_id()
+            self.logger.info(camera_id)
+            with vmb.get_camera_by_id(camera_id) as cam:
+                self.cam = cam
+                self.make = self.cam.get_name()
+                self.model = self.cam.get_model()
+                self.serial = self.cam.get_serial()
+                for feature in self.cam.get_all_features():
+                    self.logger.debug(feature.get_name())
+                while True:
+                    await asyncio.sleep(1)
+
+    async def _measure(self):
+        x1 = np.zeros(self._channel_shapes["mean"], dtype="uint")
+        x2 = x1.copy()
+        N = self._state["nframes"]
+        time1 = time.time()
+        for frame in self.cam.get_frame_generator(limit=N, timeout_ms=3000):
+            arr = frame.as_numpy_ndarray()[:,:,0].astype("uint16")
+            x1 += arr
+            x2 += arr**2
+        time4 = time.time()
+        self.logger.info(f"loop {time4-time1:0.2f}")
+        if N < 2:
+            x2.fill(np.nan)
+            stdev = x2
+        else:
+            stdev = x2 - x1**2 / N
+            stdev /= N-1
+            stdev **= 0.5
+        return {"mean": x1 / N, "stdev": stdev}
+
+    def set_exposure_time(self, time:float):
+        self.cam.get_feature_by_name("ExposureTime").set(time * 1e3)
+
+    def get_exposure_time(self) -> float:
+        return self.cam.get_feature_by_name("ExposureTime").get() / 1e3
+    
+    def get_exposure_units(self):
+        return "ms"
+
+    def set_nframes(self, nframes:int):
+        self._state["nframes"] = nframes
+
+    def get_nframes(self) -> float:
+        return self._state["nframes"]
+
+    def get_temperature(self) -> float:
+        return self.cam.get_feature_by_name("DeviceTemperature").get()
+
+    def get_led_brightness(self) -> int:
+        return self.cam.get_feature_by_name("DeviceIndicatorLuminance").get()
+
+    def set_led_brightness(self, val:int):
+        self.cam.get_feature_by_name("DeviceIndicatorLuminance").set(val)
+
+    def get_gain(self) -> float:
+        return self.cam.get_feature_by_name("Gain").get()
+
+    def set_gain(self, gain:float):
+        self.cam.get_feature_by_name("Gain").set(gain)        
