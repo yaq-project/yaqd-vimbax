ab7e80f03656f67096cdfa76df4bcb22101a379b
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2023-02-21 18:16:58 -0600

    Update _triggered.py
    float64 -> float32 (nframes > 2)
    async wait for stream

--- yaqd_vimbax/_triggered.py
+++ yaqd_vimbax/_triggered.py
@@ -40,6 +40,7 @@ class Triggered(HasMeasureTrigger):
 
     async def _measure(self):
         N = self._state["nframes"]
+        start = time.time()
         if N == 1:
             try:
                 mean = self.cam.get_frame().as_numpy_ndarray()[:, :, 0]
@@ -49,20 +50,21 @@ class Triggered(HasMeasureTrigger):
                 self.logger.error(str(e))
                 raise e
         else:
-            handler = self.Handler(N, self.get_channel_shapes()["mean"])
+            handler = self.Handler(N, self.get_channel_shapes()["mean"], "uint16")
             try:
                 self.cam.start_streaming(handler=handler, buffer_count=10)
-                handler.shutdown_event.wait()
+                await asyncio.wait_for(handler.ashutdown_event.wait(), 5)
             except Exception as e:
                 self.logger.error(str(e))
                 raise e
             finally:
                 self.cam.stop_streaming()
-            mean = handler.x1 / N
+            mean = (handler.x1 / N).astype(np.float32)
             stdev = handler.x2 - handler.x1**2 / N
             stdev /= N - 1
-            stdev **= 0.5
-            self.logger.info(f"{mean.mean():0.1f} +/- {stdev.mean():0.1f}")
+            stdev = (stdev**0.5).astype(np.float32)
+        finish = time.time()
+        self.logger.info(f"took {(finish-start):0.3f} sec")
         return {"mean": mean, "stdev": stdev}
 
     def set_exposure_time(self, time: float):
@@ -99,17 +101,18 @@ class Triggered(HasMeasureTrigger):
         self.cam.get_feature_by_name("Gain").set(gain)
 
     class Handler:
-        def __init__(self, nframes, shape):
-            self.shutdown_event = threading.Event()
+        def __init__(self, nframes, shape, arrtype):
+            self.ashutdown_event = asyncio.Event()
             self.frames_remaining = nframes
             self.x1 = np.zeros(shape, dtype="f8")
             self.x2 = self.x1.copy()
+            self.arrtype = arrtype
 
         def __call__(self, cam, stream, frame):
             if not self.frames_remaining:
-                self.shutdown_event.set()
+                self.ashutdown_event.set()
                 return
-            arr = frame.as_numpy_ndarray()[:, :, 0].astype("uint16")
+            arr = frame.as_numpy_ndarray()[:, :, 0].astype(self.arrtype)
             self.x1 += arr
             self.x2 += arr**2
             self.frames_remaining -= 1
